4f9d05979228e858f1a0710b39eebb62f55e2b82
water-case <65841586+water-case@users.noreply.github.com>
2021-08-30 18:47:37 +0900

    210830 18:47

---
+++ Newlec/workspace/di/src/spring/di/entity/NewlecExam.java
@@ -0,0 +1,56 @@
+package spring.di.entity;
+
+public class NewlecExam implements Exam {
+
+	private int kor;
+	private int eng;
+	private int math;
+	private int com;
+	
+	public NewlecExam() {
+		this(0,0,0,0);
+	}
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		super();
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
+	@Override
+	public int total() {
+		// TODO Auto-generated method stub
+		return kor+eng+math+com;
+	}
+
+	@Override
+	public float avg() {
+		// TODO Auto-generated method stub
+		return total() / 4.0f;
+	}
+
+	@Override
+	public String toString() {
+		return "NewlecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	}
+	
+	
+
+}
