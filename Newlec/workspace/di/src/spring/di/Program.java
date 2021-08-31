b7cb8df6d2ea3caa7b054f57ca3c0a34df2b68fa
water-case <65841586+water-case@users.noreply.github.com>
2021-08-31 20:07:13 +0900

    210831 20:07

--- Newlec/workspace/di/src/spring/di/Program.java
+++ Newlec/workspace/di/src/spring/di/Program.java
@@ -1,13 +1,9 @@
 package spring.di;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import spring.di.entity.Exam;
-import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -20,19 +16,22 @@ public class Program {
 		console.setExam(exam);
 		*/
 		
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+//		ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(NewlecDIConfig.class);
 		
-		Exam exam = context.getBean(Exam.class);
-		System.out.println(exam.toString());
-//		ExamConsole console = (ExamConsole) context.getBean("console");
-		ExamConsole console = context.getBean(ExamConsole.class);
+//		Exam exam = context.getBean(Exam.class);
+//		System.out.println(exam.toString());
+		
+		ExamConsole console = (ExamConsole) context.getBean("console");
+//		ExamConsole console = context.getBean(ExamConsole.class);
 		console.print();
 
-		List<Exam> exams = (List<Exam>) context.getBean("exams");//new ArrayList<>();
-		exams.add(new NewlecExam(1,1,1,1));
-		
-		for(Exam e : exams)
-			System.out.println(e);
+		/*
+		 * List<Exam> exams = (List<Exam>) context.getBean("exams");//new ArrayList<>();
+		 * exams.add(new NewlecExam(1,1,1,1));
+		 * 
+		 * for(Exam e : exams) System.out.println(e);
+		 */
 	}
 
 }
--- Newlec/workspace/di/src/spring/di/entity/NewlecExam.java
+++ Newlec/workspace/di/src/spring/di/entity/NewlecExam.java
@@ -1,10 +1,18 @@
 package spring.di.entity;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NewlecExam implements Exam {
 
+	@Value("10")
 	private int kor;
+	@Value("20")
 	private int eng;
+	@Value("30")
 	private int math;
+	@Value("40")
 	private int com;
 	
 	public NewlecExam() {
@@ -21,29 +29,35 @@ public class NewlecExam implements Exam {
 	public void setKor(int kor) {
 		this.kor = kor;
 	}
-
 	public void setEng(int eng) {
 		this.eng = eng;
 	}
-
 	public void setMath(int math) {
 		this.math = math;
 	}
-
 	public void setCom(int com) {
 		this.com = com;
 	}
 
 	@Override
 	public int total() {
-		// TODO Auto-generated method stub
-		return kor+eng+math+com;
+		
+		
+		
+		int result = kor+eng+math+com;;
+		
+		
+		
+		
+		return result;
 	}
 
 	@Override
 	public float avg() {
-		// TODO Auto-generated method stub
-		return total() / 4.0f;
+		
+		float result = total() / 4.0f; 
+		
+		return result;
 	}
 
 	@Override
